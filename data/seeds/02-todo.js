a95c0397bc1c40e35820e3f7f0584795bb8b9e7e
Nick Perez <nick@Nicks-MacBook-Pro.local>
2020-06-19 00:56:33 -0700

    added final routes

--- data/seeds/02-todo.js
+++ data/seeds/02-todo.js
@@ -2,8 +2,9 @@
 exports.seed = function(knex) {
       return knex('todos').insert([
         {title: 'test',description: "test", completed: true,  user_id: 1 },
-        {title: 'test1',description: "test1", completed: true,  user_id: 2 },
-        {title: 'test1',description: "test1", completed: false,  user_id: 2 }
+        {title: 'test1',description: "test2", completed: true,  user_id: 2 },
+        {title: 'test2',description: "test2", completed: false,  user_id: 2 },
+        {title: 'test3',description: "test3", completed: false,  user_id: 3 }
       
       ]);
    
