99d0d8bc29cd0c31942d03e2c85073011a4e3c5c
Owner <owner@Users-MacBook-Pro.local>
2020-06-21 17:07:21 -0400

    cookie-parser package added. auth.js complete with login capabilities included cookieparsing/jsonwebtoken tested on local host and functional. authorized.js filed created in middleware folder. Checks to see if user is logged in for calls. tested with get('/users') and removed after the fact. functional. needs to be added to routes depending on authorization needs as middleware.

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const server = express();
 const routes = require("./routes");
 const authRoutes = require("../auth/auth.js");
 
+const cookieParcer = require("cookie-parser");
+
 server.use(express.json());
+server.use(cookieParcer());
 server.use("/", routes);
 server.use("/api/auth", authRoutes);
 
